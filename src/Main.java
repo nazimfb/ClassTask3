3444b49567ace161371b0cd28b1a222767ddee02
nazimfb <nazimfb@code.edu.az>
2023-12-15 13:43:09 +0400

    some changes

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        String a = "12345";
-        String b = "123456";
+        String a = "125";
+        String b = "125";
         System.out.println(Toplama.toplama(a,b));
     }
 }
--- src/Toplama.java
+++ src/Toplama.java
@@ -3,23 +3,27 @@ import java.math.BigInteger;
 
 public class Toplama {
     static BigDecimal toplama (String a, String b) {
-        int[] reqemler1 = new int[a.length()];
-        int[] reqemler2 = new int[b.length()];
-        BigDecimal sumReqem;
+        BigDecimal result = null;
+        int digit1, digit2;
 
-        for (int i = 0; i < a.length(); i++) {
-            reqemler1[i] = Integer.parseInt(String.valueOf(a.charAt(i)));
-        }
-        for (int i = 0; i < b.length(); i++) {
-            reqemler2[i] = Integer.parseInt(String.valueOf(b.charAt(i)));
-        }
-        int maxLenght = Math.max(a.length(), b.length());
+        int sum;
+        int cem = 0;
+        int tmp = 0;
+        String str = "";
+        for (int i = 0; i <  Math.min(a.length(), b.length()); i++) {
+            digit1 = Integer.parseInt(String.valueOf(a.charAt(a.length() - i - 1)));
+            digit2 = Integer.parseInt(String.valueOf(b.charAt(b.length() - i - 1)));
 
-        int reqem1, reqem2;
-        while () {
-
+            sum = digit1 + digit2;
+            tmp = sum/10; //soldaki (yadda)
+            sum %= 10; //sagdaki
+            sum += tmp;
+            str = str + String.valueOf(sum);
         }
-        sumReqem = new BigDecimal();
-        return sumReqem;
+
+        System.out.println(new StringBuilder(str).reverse().toString());
+        System.out.println();
+
+        return result;
     }
 }
